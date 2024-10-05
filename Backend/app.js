6140c91c8a50519a02c45107a8f88325ca229594
Grace Chen <75271809+gracexichen@users.noreply.github.com>
2024-10-05 01:00:05 -0700

    prep deploy backend

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const { deleteStaleImages } = require("./scripts/cleanup");
+require("./bin/www");
 
 setInterval(deleteStaleImages, 10 * 1000);
 
